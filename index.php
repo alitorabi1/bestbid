348d4d853164d4ef57d546b9610ddc20328cde64
Ali Torabi <alitorabi1@gmail.com>
2016-10-09 21:03:37 -0700

    --Ali-- 09 Oct 2016 --Ali--

--- index.php
+++ index.php
@@ -13,98 +13,13 @@ $log = new Logger('main');
 $log->pushHandler(new StreamHandler('logs/everything.log', Logger::DEBUG));
 $log->pushHandler(new StreamHandler('logs/errors.log', Logger::ERROR));
 
-
-/*
-// Facebook Login block
-
-// INCLUSION OF LIBRARY FILES
-	require_once('lib/Facebook/FacebookSession.php');
-	require_once('lib/Facebook/FacebookRequest.php');
-	require_once('lib/Facebook/FacebookResponse.php');
-	require_once('lib/Facebook/FacebookSDKException.php');
-	require_once('lib/Facebook/FacebookRequestException.php');
-	require_once('lib/Facebook/FacebookRedirectLoginHelper.php');
-	require_once('lib/Facebook/FacebookAuthorizationException.php');
-	require_once('lib/Facebook/GraphObject.php');
-	require_once('lib/Facebook/GraphUser.php');
-	require_once('lib/Facebook/GraphSessionInfo.php');
-	require_once('lib/Facebook/Entities/AccessToken.php');
-	require_once('lib/Facebook/HttpClients/FacebookCurl.php');
-	require_once('lib/Facebook/HttpClients/FacebookHttpable.php');
-	require_once('lib/Facebook/HttpClients/FacebookCurlHttpClient.php');
-
-// USE NAMESPACES
-	
-	use Facebook\FacebookSession;
-	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\FacebookResponse;
-	use Facebook\FacebookSDKException;
-	use Facebook\FacebookRequestException;
-	use Facebook\FacebookAuthorizationException;
-	use Facebook\GraphObject;
-	use Facebook\GraphUser;
-	use Facebook\GraphSessionInfo;
-	use Facebook\FacebookHttpable;
-	use Facebook\FacebookCurlHttpClient;
-	use Facebook\FacebookCurl;
-
-//PROCESS
-
-	//check if users wants to logout
-	 if(isset($_REQUEST['logout'])){
-	 	unset($_SESSION['fb_token']);
-	 }
-	
-	//2.Use app id,secret and redirect url 
-	$app_id = '704374739718815';
-	$app_secret = '3cf65c2f0c77bde0f9019fc07d3c0471';
-	$redirect_url='http://bestbid.ipd8.info/';
-
-	//3.Initialize application, create helper object and get fb sess
-	 FacebookSession::setDefaultApplication($app_id,$app_secret);
-	 $helper = new FacebookRedirectLoginHelper($redirect_url);
-	 $sess = $helper->getSessionFromRedirect();
-
-	//check if facebook session exists
-	if(isset($_SESSION['fb_token'])){
-	 	$sess = new FacebookSession($_SESSION['fb_token']);
-	}
-
-	//4. if fb sess exists echo name 
-	 	if(isset($sess)){
-	 		//store the token in the php session
-	 		$_SESSION['fb_token']=$sess->getToken();
-	 		//create request object,execute and capture response
-	 		$request = new FacebookRequest($sess,'GET','/me');
-			// from response get graph object
-			$response = $request->execute();
-			$graph = $response->getGraphObject(GraphUser::classname());
-			// use graph object methods to get user details
-			$name = $graph->getName();
-			$id = $graph->getId();
-			$image = 'https://graph.facebook.com/'.$id.'/picture?width=300';
-			$email = $graph->getProperty('email');
-			echo "hi $name <br>";
-			echo "your email is $email <br><Br>";
-			echo "<img src='$image' /><br><br>";
-			echo "<a href='/logout'><button>Logout</button></a>";
-	 	}else{
-			//else echo login
-	 		echo '<a href="'.$helper->getLoginUrl(array('email')).'" >Login with facebook</a>';
-	 	}
-
-// Facebook Login block
-*/
-
-
-
+//DB::$dbName = 'cp4724_bestbid';
+//DB::$user = 'cp4724_bestbid';
 DB::$dbName = 'bestbid';
 DB::$user = 'bestbid';
 //DB::$password = '9uYCYW2r8xDQfZvJ';   //JAC
 DB::$password = 'bDYeWvRqrfzL6wDe'; //Home
 DB::$encoding = 'utf8'; // defaults to latin1 if omitted
-
 // DB::$host = '127.0.0.1'; // sometimes needed on Mac OSX
 DB::$error_handler = 'sql_error_handler';
 DB::$nonsql_error_handler = 'nonsql_error_handler';
@@ -143,9 +58,8 @@ if (!isset($_SESSION['user'])) {
     $_SESSION['user'] = array();
 }
 
-$app->get('/', function() use ($app) {    
-    $app->render('index.html.twig',
-            array('sessionUser' => $_SESSION['user']));
+$app->get('/', function() use ($app) {
+    $app->render('index.html.twig', array('sessionUser' => $_SESSION['user']));
 });
 
 $app->get('/userexists/:username', function($username) use ($app, $log) {
@@ -154,6 +68,35 @@ $app->get('/userexists/:username', function($username) use ($app, $log) {
         echo "<font size='2' color='red'>&nbsp;&nbsp;User already registered</font>";
     }
 });
+
+//facebook login
+$fbID = "704374739718815";
+$fbPass = "3cf65c2f0c77bde0f9019fc07d3c0471";
+$fb = new Facebook\Facebook([
+    'app_id' => $fbID,
+    'app_secret' => $fbPass,
+    'default_graph_version' => 'v2.5',
+    'persistent_data_handler' => 'session'
+        ]);
+
+$helper = $fb->getRedirectLoginHelper();
+$permissions = ['public_profile', 'email', 'user_location']; // optional
+
+$loginUrl = $helper->getLoginUrl('http://bestbid.ipd8.info/fblogin.php', $permissions);
+$logoutUrl = $helper->getLogoutUrl('http://bestbid.ipd8.info/fblogout.php', $permissions);
+
+$fbUser = array();
+if (isset($_SESSION['facebook_access_token'])) {
+    $fbUser = $_SESSION['facebook_access_token'];
+}
+
+$twig = $app->view()->getEnvironment();
+$twig->addGlobal('fbUser', $fbUser);
+$twig->addGlobal('loginUrl', $loginUrl);
+$twig->addGlobal('logoutUrl', $logoutUrl);
+
+//print_r($fbUser);
+//print_r($_SESSION['fbmetadata']);
 
 // State 1: first show
 $app->get('/register', function() use ($app, $log) {
@@ -169,7 +112,7 @@ $app->post('/register', function() use ($app, $log) {
     $country = $app->request->post('country');
     $pass1 = $app->request->post('pass1');
     $pass2 = $app->request->post('pass2');
-    $valueList = array ('username' => $username, 'email' => $email, 'address' => $address, 'codepostal' => $codepostal, 'state' => $state, 'country' => $country);
+    $valueList = array('username' => $username, 'email' => $email, 'address' => $address, 'codepostal' => $codepostal, 'state' => $state, 'country' => $country);
     // submission received - verify
     $errorList = array();
     if (!$email || !$codepostal || !$pass1 || !$pass2) {
@@ -183,25 +126,25 @@ $app->post('/register', function() use ($app, $log) {
         array_push($errorList, "Email does not look like a valid email");
         unset($valueList['email']);
     } else {
-        $user = DB::queryFirstRow("SELECT ID FROM users WHERE email=%s", $email);        
+        $user = DB::queryFirstRow("SELECT ID FROM users WHERE email=%s", $email);
         if ($user) {
             array_push($errorList, "User already registered");
             unset($valueList['email']);
         }
     }
-    $ZIPREG=array(
-	"US"=>"^\d{5}([\-]?\d{4})?$",
-	"UK"=>"^(GIR|[A-Z]\d[A-Z\d]??|[A-Z]{2}\d[A-Z\d]??)[ ]??(\d[A-Z]{2})$",
-	"DE"=>"\b((?:0[1-46-9]\d{3})|(?:[1-357-9]\d{4})|(?:[4][0-24-9]\d{3})|(?:[6][013-9]\d{3}))\b",
-	"CA"=>"^([ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[ABCEGHJKLMNPRSTVWXYZ]\d)$",
-	"FR"=>"^(F-)?((2[A|B])|[0-9]{2})[0-9]{3}$",
-	"IT"=>"^(V-|I-)?[0-9]{5}$",
-	"AU"=>"^(0[289][0-9]{2})|([1345689][0-9]{3})|(2[0-8][0-9]{2})|(290[0-9])|(291[0-4])|(7[0-4][0-9]{2})|(7[8-9][0-9]{2})$",
-	"NL"=>"^[1-9][0-9]{3}\s?([a-zA-Z]{2})?$",
-	"ES"=>"^([1-9]{2}|[0-9][1-9]|[1-9][0-9])[0-9]{3}$",
-	"DK"=>"^([D-d][K-k])?( |-)?[1-9]{1}[0-9]{3}$",
-	"SE"=>"^(s-|S-){0,1}[0-9]{3}\s?[0-9]{2}$",
-	"BE"=>"^[1-9]{1}[0-9]{3}$"
+    $ZIPREG = array(
+        "US" => "^\d{5}([\-]?\d{4})?$",
+        "UK" => "^(GIR|[A-Z]\d[A-Z\d]??|[A-Z]{2}\d[A-Z\d]??)[ ]??(\d[A-Z]{2})$",
+        "DE" => "\b((?:0[1-46-9]\d{3})|(?:[1-357-9]\d{4})|(?:[4][0-24-9]\d{3})|(?:[6][013-9]\d{3}))\b",
+        "CA" => "^([ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[ABCEGHJKLMNPRSTVWXYZ]\d)$",
+        "FR" => "^(F-)?((2[A|B])|[0-9]{2})[0-9]{3}$",
+        "IT" => "^(V-|I-)?[0-9]{5}$",
+        "AU" => "^(0[289][0-9]{2})|([1345689][0-9]{3})|(2[0-8][0-9]{2})|(290[0-9])|(291[0-4])|(7[0-4][0-9]{2})|(7[8-9][0-9]{2})$",
+        "IR" => "^[1-9]{1}[0-9]{5}$",
+        "ES" => "^([1-9]{2}|[0-9][1-9]|[1-9][0-9])[0-9]{3}$",
+        "DK" => "^([D-d][K-k])?( |-)?[1-9]{1}[0-9]{3}$",
+        "SE" => "^(s-|S-){0,1}[0-9]{3}\s?[0-9]{2}$",
+        "BE" => "^[1-9]{1}[0-9]{3}$"
     );
     if (strlen($country) < 2 || !array_key_exists($country, $ZIPREG)) {
         array_push($errorList, "Please enter a country from the list");
@@ -232,11 +175,11 @@ $app->post('/register', function() use ($app, $log) {
         ));
     } else {
         // STATE 2: submission successful
-        DB::insert('users', array('username' => $username, 'email' => $email, 
+        DB::insert('users', array('username' => $username, 'email' => $email,
             'password' => password_hash($pass1, CRYPT_BLOWFISH),
-            'address' => $address, 'codepostal' => $codepostal, 
+            'address' => $address, 'codepostal' => $codepostal,
             'state' => $state, 'country' => $country
-            // 'password' => hash('sha256', $pass1)
+                // 'password' => hash('sha256', $pass1)
         ));
         $id = DB::insertId();
         $log->debug(sprintf("User %s created", $id));
@@ -254,8 +197,7 @@ $app->post('/login', function() use ($app, $log) {
     $pass = $app->request->post('pass');
     $user = DB::queryFirstRow("SELECT * FROM users WHERE username=%s", $username);
     if (!$user) {
-        $log->debug(sprintf("User failed for username %s from IP %s",
-                    $username, $_SERVER['REMOTE_ADDR']));
+        $log->debug(sprintf("User failed for username %s from IP %s", $username, $_SERVER['REMOTE_ADDR']));
         $app->render('login.html.twig', array('loginFailed' => TRUE));
     } else {
 //echo "<pre>\n";
@@ -269,13 +211,11 @@ $app->post('/login', function() use ($app, $log) {
             // LOGIN successful
             unset($user['password']);
             $_SESSION['user'] = $user;
-            $log->debug(sprintf("User %s logged in successfuly from IP %s",
-                    $user['ID'], $_SERVER['REMOTE_ADDR']));
-            $app->render('login_success.html.twig');
+            $log->debug(sprintf("User %s logged in successfuly from IP %s", $user['ID'], $_SERVER['REMOTE_ADDR']));
+            $app->render('index.html.twig', array('sessionUser' => $_SESSION['user']));
         } else {
-            $log->debug(sprintf("User failed for username %s from IP %s",
-                    $username, $_SERVER['REMOTE_ADDR']));
-            $app->render('login.html.twig', array('loginFailed' => TRUE));            
+            $log->debug(sprintf("User failed for username %s from IP %s", $username, $_SERVER['REMOTE_ADDR']));
+            $app->render('login.html.twig', array('loginFailed' => TRUE));
         }
     }
 });
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit2c9dbcda92f111aa6afdbcef5e23ed1e
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit2c9dbcda92f111aa6afdbcef5e23ed1e::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire2c9dbcda92f111aa6afdbcef5e23ed1e($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire2c9dbcda92f111aa6afdbcef5e23ed1e($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
